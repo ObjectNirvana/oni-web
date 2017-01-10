903155a15d40abe5496757ef7d8c60d91b24745e
Michael McCray <mike@objectnirvana.com>
2017-01-10 08:11:08 -0600

    fix git

--- shared/src/main/scala/com/oni/udash/rpc/MainClientRPC.scala
+++ shared/src/main/scala/com/oni/udash/rpc/MainClientRPC.scala
@@ -3,6 +3,9 @@ package com.oni.udash.rpc
 import com.avsystem.commons.rpc.RPC
 import io.udash.rpc._
 
+/**
+ * interface from server to client
+ */
 @RPC
 trait MainClientRPC {
   def push2(number: Int): Unit
