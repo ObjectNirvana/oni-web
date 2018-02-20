bb41649d4de025b3018540b5ff772645e1b00246
Michael D. McCray <mike@objectnirvana.com>
2018-02-19 22:46:48 -0600

    fix port

--- backend/src/main/scala/com/oni/udash/Launcher.scala
+++ backend/src/main/scala/com/oni/udash/Launcher.scala
@@ -4,7 +4,7 @@ import com.oni.udash.jetty.ApplicationServer
 
 object Launcher {
   def main(args: Array[String]): Unit = {
-    val port = 8080
+    val port = 12013
     val server = new ApplicationServer(port, "backend/target/UdashStatic/WebContent")
     server.start()
   }
